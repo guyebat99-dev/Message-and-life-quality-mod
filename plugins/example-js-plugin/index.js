1cb35c4258f793910ce5d06c7ce861001197139a
guyebat99-dev <guyebat99@gmail.com>
2026-08-13 18:25:34 +0400

    Create index.js

---
+++ plugins/example-js-plugin/index.js
@@ -0,0 +1,20 @@
+(function (exports, jsxRuntime) {
+    Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+
+    var pluginDefinition = {
+        start: function () {
+            console.log("[message-tools] Plugin started successfully!");
+        },
+        stop: function () {
+            console.log("[message-tools] Plugin stopped successfully!");
+        }
+    };
+
+    if (typeof plugin === 'function') {
+        exports.default = plugin(pluginDefinition);
+    } else {
+        exports.default = pluginDefinition;
+    }
+
+    return exports;
+})({}, revenge.react.ReactJSXRuntime);
